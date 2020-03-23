9a708bdfb1608c5c674566629a3fbf2fad6399b3
Luke Andrews <andrews.lu@husky.neu.edu>
2020-03-23 13:02:22 -0400

    Add comments detailing the approach we're taking

--- src/cs3500/excellence/Excellence.java
+++ src/cs3500/excellence/Excellence.java
@@ -56,5 +56,22 @@ public final class Excellence {
     }
     AnimationController controller = new Controller(viewType, speed, in, out);
     controller.animate(model);
+
+    // Spin up a model (already spun up above)
+    // Translate the fileName given into a Readable file
+    // Spin up a new animation builder
+    // Discover from the input what type of view these bastards want
+    // Proceed to pass the readable file and the model (which will need a delete method, and
+    // will need to implement the new AnimationBuilder interface) to the AnimationBuilder
+
+    // Then, beautifully, the AnimationBuilder just goes ahead and automatically applies all of the
+    // instructions provided in the file in question to the model via the methods we already
+    // implemented.
+
+    // Once this process is done, we are still in the main method (have been the whole time)
+    // then we simply pass the model, which now has all the shapes and motions from the file,
+    // courtesy of the AnimationBuilder, to whatever view choice the Main method specifies.
+
+    // Tell the view to execute.
   }
 }
