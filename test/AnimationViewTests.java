46991110d6588e7902c007894232ebdd42608f9d
allenwest24 <west.all@husky.neu.edu>
2020-03-26 09:00:52 -0400

    Added proposed list of tests I'm going to write after we figure out the bugs.

--- test/AnimationViewTests.java
+++ test/AnimationViewTests.java
@@ -1,5 +1,6 @@
 import org.junit.Test;
 
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 
 import cs3500.excellence.AnimationDelegate;
@@ -14,5 +15,16 @@ import static org.junit.Assert.assertTrue;
  * Test class for the IView interface, which contains the public methods needed for the view.
  */
 public class AnimationViewTests {
+  // Test constructors IView Constructors x3
+  // Test stringOutputForFile() for visual.
+  // Test stringOutputForFile() for text.
+  // Test stringOutputForFile() for svg.
+  // Test refresh() for visual.
+  // Test refresh() for text.
+  // Test refresh() for svg.
+  // Test makeVisible() for visual.
+  // Test makeVisible() for text.
+  // Test makeVisible() for svg.
+  // Test actionPerformed(ActionEvent e) for visual.
 
 }
